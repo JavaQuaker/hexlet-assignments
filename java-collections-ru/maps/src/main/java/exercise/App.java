d4bfbaf17be165209874669365ce00d63da86cb7
@hexlet/cli <support@hexlet.io>
2023-06-28 00:01:38 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -1,10 +1,13 @@
 package exercise;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 // BEGIN
 public class App {
+    static String sentence1 = "word text cat apple word map apple word";
     public static Map  getWordCount(String sentence) {
         Map<String, Integer> map = new HashMap<>();
         String[] str = sentence.replaceAll("(^\\s+|\\s+$)", "").split("\\s+");
@@ -25,12 +28,16 @@ public class App {
             StringBuilder str = new StringBuilder();
             System.out.println("{");
             for (Object key : map.keySet()) {
-                str.append("  " + key + ":" + " " + map.get(key) + "\n");
+                str.append(" ").append(key).append(":" + " ").append(map.get(key)).append("\n");
             }
-            System.out.println(str);
+            System.out.println(str.toString().replaceAll("\\s+$", ""));
             System.out.println("}");
-            return str.toString().trim();
+            return str.toString();
         }
+
+    public static void main(String[] args) {
+        toString(getWordCount(sentence1));
+    }
 }
 
 //END

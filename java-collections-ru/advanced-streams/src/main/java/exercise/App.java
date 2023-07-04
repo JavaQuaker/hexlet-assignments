c844d4d0380c37ef87df69696b0d54ed8f7a2012
@hexlet/cli <support@hexlet.io>
2023-07-04 19:31:32 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -5,29 +5,29 @@ import java.util.Arrays;
 
 // BEGIN
 public class App {
-
+//    static String expected2 = "X_FORWARDED_MAIL=tirion@google.com,X_FORWARDED_HOME=/home/tirion,language=en";
+    static String expected2 = "key5=value5,X_FORWARDED_var3=value,key6=value6";
     public static String getForwardedVariables(String environment) {
-
-        StringBuilder builder = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         String[] str = environment.split(",");
-        System.out.println(Arrays.toString(str));
-
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].contains("X_FORWARDED_")) {
-                String result = str[i].replace("X_FORWARDED_", "").trim();
-
-                builder.append(result + ",");
+            result.append("\"");
+        for (String q : str) {
+            if (q.contains("X_FORWARDED_")) {
+                String w = q.replaceAll("X_FORWARDED_", "");
+                result.append(w);
+                result.append(",");
             }
         }
-        return "\"" + removeLastChar(builder) + "\"";
+        return removeLastChar(result).toString() + "\"";
     }
-
     public static StringBuilder removeLastChar(StringBuilder builder) {
         if (builder == null || builder.length() == 0) {
             return builder;
         }
         return new StringBuilder(builder.substring(0, builder.length() - 1));
     }
-}
 
-//END
+    public static void main(String[] args) {
+        System.out.println(getForwardedVariables(expected2));
+    }
+}

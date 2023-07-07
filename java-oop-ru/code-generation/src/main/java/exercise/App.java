ce13dd505afa68a9ea83019263fea1b8006e14ea
@hexlet/cli <support@hexlet.io>
2023-07-07 18:33:15 +0300

    submit java-oop-ru/code-generation

--- java-oop-ru/code-generation/src/main/java/exercise/App.java
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -3,11 +3,12 @@ package exercise;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.LinkedHashMap;
@@ -15,27 +16,28 @@ import java.util.Map;
 
 // BEGIN
 public class App {
-    public static void main(String[] args) {
-        Map<String, Object> owner = new LinkedHashMap<>();
+    public static void main(String[] args) throws JsonProcessingException {
+      Map<String, Object> owner = new LinkedHashMap<>();
       User user = new User(1, "Ivan", "Ivanov", 50 );
       Car car = new Car(1, "bmv", "x6", "white", user);
-        App.save(Path.of("java-oop-ru/code-generation/src/test/resources/testCarJson.json"), car);
+      App.save(Path.of("src/test/resources/testCarJson.json"), car);
     }
-    public static void save(Path path, Car car) {
+    public static void save(Path path, Car car) throws JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         String filePath = String.valueOf(path);
         File file = new File(filePath);
 
         try {
-           String result = objectMapper.writeValueAsString(car);
-           FileWriter fileWriter = new FileWriter(file);
+            String result = objectMapper.writeValueAsString(car);
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(result);
+            fileWriter.flush();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
     }
-    public static Car extract(Path path) {
+        public static Car extract(Path path) {
         Car car;
         String str = String.valueOf(path);
         File file = new File(str);

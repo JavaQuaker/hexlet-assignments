218f709a047baa10f3b9d81e7e61e500bb94682d
@hexlet/cli <support@hexlet.io>
2023-09-01 16:00:34 +0300

    submit java-web-ru/cookies_01.09.23_12.12.11

---
+++ java-web-ru/cookies_01.09.23_12.12.11/src/main/java/exercise/App.java
@@ -0,0 +1,32 @@
+package exercise;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exercise.dto.CartPage;
+import io.javalin.Javalin;
+import exercise.controller.CartsController;
+import exercise.util.NamedRoutes;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get(NamedRoutes.rootPath(), CartsController::index);
+        // BEGIN
+        app.post(NamedRoutes.addItemPath(), CartsController::addCart);
+        app.post(NamedRoutes.clearCartPath(), CartsController::removeCount);
+        // END
+
+
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}

93424e118e167c7118620d0ef118b313337c2eee
@hexlet/cli <support@hexlet.io>
2023-11-01 15:19:13 +0300

    submit java-spring-ru/queues

---
+++ java-spring-ru/queues/consumer/src/main/java/exercise/controller/MessageController.java
@@ -0,0 +1,23 @@
+package exercise.controller;
+
+import exercise.QueueListener;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+    private final QueueListener listener;
+
+    MessageController(QueueListener listener) {
+        this.listener = listener;
+    }
+
+    @GetMapping(path = "")
+    public List<String> getAllMessages() {
+        return listener.getAllMessages();
+    }
+}
